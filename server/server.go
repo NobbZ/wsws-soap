1b1ff214b8fd0d167367fbebc730ba558959251c
Norbert Melzer <timmelzer@gmail.com>
2017-05-02 23:46:13 +0200

    Adding a bit of documentation
    This is because the nagging of gometalinter

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import "net/http"
 
+// Start starts a server listening on `addr` for incomming requests.
 func Start(addr string) {
 	http.HandleFunc("/stockquote", handler)
 	http.ListenAndServe(addr, nil)
