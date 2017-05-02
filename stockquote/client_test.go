cbb2f318ca8949171db7c5253d6e95d705c8d354
Norbert Melzer <timmelzer@gmail.com>
2017-05-02 23:37:42 +0200

    use a fresh server in the test

--- stockquote/client_test.go
+++ stockquote/client_test.go
@@ -3,13 +3,19 @@ package stockquote_test
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/NobbZ/wsws-soap/server"
 	"github.com/NobbZ/wsws-soap/stockquote"
 )
 
 func TestStockQuote(t *testing.T) {
-	service := stockquote.NewStockQuotePortType("http://localhost:8080/stockquote", false, nil)
+	go server.Start("127.0.0.1:9999")
+	time.Sleep(10 * time.Millisecond) // Wait a bit for the server to start
+
+	service := stockquote.NewStockQuotePortType("http://localhost:9999/stockquote", false, nil)
 	seasons, err := service.GetLastTradePrice(&stockquote.TradePriceRequest{TickerSymbol: "SIE"})
+
 	if err != nil {
 		panic(err)
 	}
