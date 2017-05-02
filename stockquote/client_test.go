e93d5d2c902f4cf0e38a0ab5ff028e0f8f25cfe8
Norbert Melzer <timmelzer@gmail.com>
2017-05-02 23:01:55 +0200

    Add a very basic test, which actually tests something

--- stockquote/client_test.go
+++ stockquote/client_test.go
@@ -7,11 +7,14 @@ import (
 	"github.com/NobbZ/wsws-soap/stockquote"
 )
 
-func TestStockQuote(*testing.T) {
+func TestStockQuote(t *testing.T) {
 	service := stockquote.NewStockQuotePortType("http://localhost:8080/stockquote", false, nil)
 	seasons, err := service.GetLastTradePrice(&stockquote.TradePriceRequest{TickerSymbol: "SIE"})
 	if err != nil {
 		panic(err)
+	}
+	if seasons.Price != 5.0 {
+		t.Error("Expected", 5.0, "but got", seasons.Price)
 	}
 	fmt.Printf("Trade Price: %+v\n", seasons)
 }
